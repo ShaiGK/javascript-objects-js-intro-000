3f346cf11989ac3421adb7e09c085490abcec527
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-04-20 20:31:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-function removeFromPlaylist(playlist, artistName) {delete playlist.artistName}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName}
